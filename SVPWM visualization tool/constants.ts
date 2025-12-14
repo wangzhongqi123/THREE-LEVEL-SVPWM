7db490144d8819d9ab3e2d1e5782719844851231
Wang Zhongqi <2273651827@qq.com>
2025-12-15 04:45:43 +0800

    Remove SVPWM_visualization_tool folder

--- SVPWM visualization tool/constants.ts
+++
@@ -1,7 +0,0 @@
-export const PI = Math.PI;
-export const TWO_PI = 2 * Math.PI;
-export const PI_DIV_3 = Math.PI / 3;
-export const PI_DIV_6 = Math.PI / 6;
-export const SQRT_3 = Math.sqrt(3);
-export const SQRT_3_DIV_3 = Math.sqrt(3) / 3;
-export const SQRT_3_DIV_6 = Math.sqrt(3) / 6;
